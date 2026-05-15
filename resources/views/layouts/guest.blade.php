93a4fec5bb76657a5bbd9b91aa6bb093a6e65d9e
friday anaja <anajafriday113@gmail.com>
2026-05-16 00:08:18 +0100

    added google analytics

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,7 +10,16 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <meta name="google-site-verification" content="-hBPQH4s3IQb9n_i5_oh5KOyOI_ey-P5Q6oP5p9MhXE" />
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2SVWJ1TLGX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-2SVWJ1TLGX');
+</script>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
